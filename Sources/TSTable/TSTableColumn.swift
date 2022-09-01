12eec6f1396cec5122e4a74604979c0ffb0d3f7a
todayssky <rudwns1113@gmail.com>
2022-09-01 16:55:59 +0900

    fix access control

--- Sources/TSTable/TSTableColumn.swift
+++ Sources/TSTable/TSTableColumn.swift
@@ -11,4 +11,14 @@ public struct TSTableColumn<T>: Hashable where T: Hashable {
     public let name: String
     public let value: KeyPath<T, String>
     public var widthRatio: CGFloat?
+    
+    public init(
+        name: String,
+        value: KeyPath<T, String>,
+        widthRatio: CGFloat? = nil
+    ) {
+        self.name = name
+        self.value = value
+        self.widthRatio = widthRatio
+    }
 }
